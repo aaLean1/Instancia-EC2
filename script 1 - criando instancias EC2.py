ba66154a6464d2fe71257a6383adfe358a60fb20
Allan Cicero <104519992+aaLean1@users.noreply.github.com>
2023-03-01 14:12:00 -0300

    Update script 1 - criando instancias EC2.py

--- script 1 - criando instancias EC2.py
+++ script 1 - criando instancias EC2.py
@@ -26,20 +26,21 @@
 
 
 
-
 # Script da instancia
 
 import os
 import boto3
 
+#criação das 4 variaveis necessarias para a criação da instancia EC2
 AMI = os.environ['AMI']
 INSTANCE_TYPE = os.environ['INSTANCE_TYPE']
 KEY_NAME = os.environ['KEY_NAME']
 SUBNET_ID = os.environ['SUBNET_ID']
 
+
 ec2 = boto3.resource('ec2')
 
-
+#Criei uma DEF onde dentro tem evento e um contexto.
 def lambda_handler(event, context):
 
     instance = ec2.create_instances(
@@ -52,3 +53,4 @@ def lambda_handler(event, context):
     )
 
     print("New instance created:", instance[0].id)
+
